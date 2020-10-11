cb21a2bb09dcf3051921a854383578770eee5168
yisongnut <71172220+yisongnut@users.noreply.github.com>
2020-10-11 19:06:25 +0900

    33
    333

---
+++ homework_02.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 사용합니다. 'dbsparta' db가 없다면 새로 만듭니다.
+
+# MongoDB에 insert 하기
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713',headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+for tr in trs:
+    rank = tr.select_one('td.number').text[0:2].strip()
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = tr.select_one('td.info > a.artist.ellipsis').text.strip()
+    print(rank, title, artist)
+
+    db.musics.insert_one({'rank':rank, 'title':title, 'artist':artist})
+
+
